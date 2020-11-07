39b9a632fe5d76dd583da2cac0ab52ae14dc9ec9
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-11-07 17:47:14 +0700

    aaaa

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,10 @@ module RailsDeviseTokenAuth
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', 
+        headers: :any, 
+        methods: [:get, :patch, :post, :options, :delete, :head],
+        expose: ['access-token', 'token-type', 'client', 'expiry', 'uid']
       end
     end
   end
